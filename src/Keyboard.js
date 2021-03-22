98d232666e2c4aca6c7cf60178b8d428d1eb63e9
aegean <47485743+egeturan@users.noreply.github.com>
2021-03-22 23:58:03 +0300

    asad

--- src/Keyboard.js
+++
@@ -1,8 +0,0 @@
-import KeyboardEventHandler from 'react-keyboard-event-handler';
-
-const Keyboard = (props) => (<div>
-  <div>key detected: {props.eventKey}</div>
-  <KeyboardEventHandler
-    handleKeys={['a', 'b', 'c']}
-    onKeyEvent={(key, e) => console.log(`do something upon keydown event of ${key}`)} />
-</div>);
